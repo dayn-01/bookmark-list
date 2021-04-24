cb60f491e92421cd32018ea6d5ba5102a12c679b
Daniel Balmoria <danielbalmoria@gmail.com>
2021-04-24 10:20:20 +0800

    v.1 done

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# manganelo-kakalot-bookmark-list
+# manganelo-mangakakalot-bookmark-list
+
+## Usage
+
+- go to https://manganelo.com/bookmark or https://mangakakalot.com/ 
+- go to Dev Tools -> Console
+- paste save_bookmarks.js to console (or just drag and drop), and press enter!
+
+
+
+
+
+### notes
+a simplified version of [this](https://greasyfork.org/en/scripts/390432-mananelo-mangakakalot-bookmarks-export/code) script
---
+++ save_bookmarks.js
@@ -0,0 +1,83 @@
+// @require      https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.8/FileSaver.min.js
+
+function addBookarkStyles(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
+ 
+addBookarkStyles(`
+#temp_data {
+position: absolute; top: -9999px;
+left: -9999px;
+}
+`);
+ 
+(function() {
+    'use strict';
+
+    let iPage, oBookMarks, sTItle, iLastView;
+    let sDomain = window.location.hostname
+
+    iPage = '.group_page a'
+    oBookMarks = '.bookmark_item'
+    sTItle = '.bookmark_title'
+    iLastView = '.bookmark_chap a'
+
+    if(sDomain === 'manganelo.com') {
+        iPage = '.group-page a'
+        oBookMarks = '.bookmark-item'
+        sTItle = '.item-story-name'
+        iLastView = '.item-title a'
+    }
+
+    let iPageCount = parseInt($(`${iPage}`).last().text().replace(/\D+/g, ''))
+ 
+    const oGetBookmarks = () => {
+        $('#temp_data').remove()
+        $('body').append("<div id='temp_data'>")
+        let pageSuccess = 0; 
+        let bookmarkedTitles = '';
+        for(var i = 0; i < iPageCount; i++) {
+            $("#temp_data").append(`<div id='page${i+1}'>`)
+            $(`#page${i+1}`).load(`https://${sDomain}/bookmark?page=${i+1} ${oBookMarks}`, (resp,status,xhr) => {
+                if(status == "success") { pageSuccess++}
+                if(pageSuccess == iPageCount) {
+                    let oItem = $(`#temp_data ${oBookMarks}`)
+                    for(var j = 0; j < oItem.length; j++) {
+                        if($(oItem[j]).find(`${sTItle}`).text()) {
+                            let sLastChap = $(oItem[j]).find(`${iLastView}`),
+                            oBookmarkItem = $(oItem[j]).find(`${sTItle}`);
+                            if(oBookmarkItem.length > 0) {
+                                bookmarkedTitles += oBookmarkItem.text() + ` | `+oBookmarkItem.attr('href') + `  | ${sLastChap.length > 0 ? sLastChap[0].text : 'Not Found' } \n`
+                            }
+                        }
+                    }
+
+                    console.log(bookmarkedTitles);
+                    oSavetoText(bookmarkedTitles, 'bookmark.txt', 'text/plain');
+                    $('#temp_data').remove()
+                }
+            })
+        }
+    }
+
+    const oSavetoText = (content, filename, contentType) => {
+        const oElement = document.createElement('a');
+        const file = new Blob([content], {type: contentType});
+        
+        oElement.href= URL.createObjectURL(file);
+        oElement.download = filename;
+        oElement.click();
+      
+          URL.revokeObjectURL(a.href);
+    };
+
+
+    oGetBookmarks();
+
+})();
